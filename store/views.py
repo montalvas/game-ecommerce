7b7de2d6d1912062cc4b4c9fe5bee03760900965
montalvas <lucas_montalvani@hotmail.com>
2022-06-01 10:10:37 -0300

    Configurações iniciais do projeto

---
+++ store/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    context = {}
+    return render(request, 'store/home.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
