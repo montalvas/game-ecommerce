12c5348e00a2d05d55981b9376a6e43e6b4d7115
montalvas <lucas_montalvani@hotmail.com>
2022-06-02 19:28:40 -0300

    Criado os models da aplicacao

--- store/models.py
+++ store/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                null=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    score = models.PositiveIntegerField(null=True, default=0)
+    image = models.ImageField(upload_to='covers', null=True, default='covers/placeholder.png')
+    
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True)
+    transaction_id = models.CharField(max_length=200, null=True)
+    shipping = models.PositiveIntegerField()
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f'Order: {self.id}'
+    
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'OrderItem: {self.id}, product: {self.product}, order: {self.order}'
+    
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.address
