dece616142d5c5fdf2d65b2b9f085c620077131c
montalvas <lucas_montalvani@hotmail.com>
2022-06-03 10:19:24 -0300

    Tornando o template dinâmico

--- store/models.py
+++ store/models.py
@@ -34,11 +34,62 @@ class Order(models.Model):
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True)
     transaction_id = models.CharField(max_length=200, null=True)
-    shipping = models.PositiveIntegerField()
+    shipping = models.PositiveIntegerField(default=0)
     total = models.DecimalField(max_digits=10, decimal_places=2)
     
     def __str__(self):
         return f'Order: {self.id}'
+    
+    @property
+    def get_quantity(self):
+        orderitems = self.orderitem_set.all()
+        if orderitems:
+            quantity = orderitems.count()
+        else:
+            quantity = 0
+        
+        return quantity
+    
+    @property
+    def get_cart_subtotal(self):
+        orderitems = self.orderitem_set.all()
+        if orderitems:
+            total = sum([item.get_price for item in orderitems])
+        else:
+            total = 0
+            
+        return total
+    
+    @property
+    def get_shipping(self):
+        orderitems = self.orderitem_set.all()
+        if orderitems:
+            total_cart = sum([item.get_price for item in orderitems])
+            if total_cart >= 250:
+                total = 0
+            else:
+                total = sum([10 for item in orderitems])
+        else:
+            total = 0
+            
+        return total
+    
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        if orderitems:
+            shipping = sum([item.get_price for item in orderitems])
+            if shipping >= 250:
+                shipping = 0
+            else:
+                shipping = sum([10 for item in orderitems])
+        
+            subtotal = sum([item.get_price for item in orderitems])
+            total = shipping + subtotal
+        else:
+            total = 0
+            
+        return total
     
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
@@ -47,6 +98,10 @@ class OrderItem(models.Model):
     
     def __str__(self):
         return f'OrderItem: {self.id}, product: {self.product}, order: {self.order}'
+    
+    @property
+    def get_price(self):
+        return self.product.price
     
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Product
+
+from .models import *
 
 # Create your views here.
 def home(request):
@@ -11,10 +12,43 @@ def home(request):
 
 
 def cart(request):
-    context = {}
+    
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,
+                                                     complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {
+            'get_quantity': 0,
+            'get_shipping': 0,
+            'get_cart_subtotal': 0,
+            'get_cart_total': 0
+        }
+    context = {
+        'items': items,
+        'order': order
+    }
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,
+                                                     complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {
+            'get_quantity': 0,
+            'get_shipping': 0,
+            'get_cart_subtotal': 0,
+            'get_cart_total': 0
+        }
+    context = {
+        'items': items,
+        'order': order
+    }
     return render(request, 'store/checkout.html', context)
